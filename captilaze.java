4ce0366ce0f63e5ea7a47a0d288bf0cf9a6d918d
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-12 06:03:41 -0700

    Update captilaze.java

--- captilaze.java
+++ captilaze.java
@@ -7,6 +7,11 @@ Scanner sc=new Scanner(System.in);
 String str=sc.nextLine();
 char[] ch=str.toCharArray();
 int len=ch.length();
+  if (ch[0]>='a' && ch[0]<='z')
+  {
+    ch[0]=(char)(ch[0]-'a'+'A');
+  }
+      
 for (int i=0;i<=len-1;i++)
 {
 if (ch[i]=='')
