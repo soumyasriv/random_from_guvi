72868242320207dffef32d93e87d5891d9d86310
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-18 06:02:59 -0700

    Update largest_no_hunter.java

--- largest_no_hunter.java
+++ largest_no_hunter.java
@@ -2,19 +2,22 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int a[]=new int[n];
-		int temp;
-		for (int i=0;i<n;i++)
-		{
-		    a[i]=sc.nextInt();
-		}
-		for (int i = 0; i < n; i++) 
+	 Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 int a[]=new int[n+1];
+	 int count=0;
+	 int temp;
+	 a[0]=-4569;
+	 for (int i=1;i<=n;i++)
+	 {
+	     a[i]=sc.nextInt();
+	 }
+	
+        for (int i = 1; i <= n; i++) 
         {
-            for (int j = i + 1; j < n; j++) 
+            for (int j = i + 1; j<=n; j++) 
             {
-                if (a[i] < a[j]) 
+                if (a[i] > a[j]) 
                 {
                     temp = a[i];
                     a[i] = a[j];
@@ -22,10 +25,22 @@ public class Main
                 }
             }
         }
-        
-        for (int i=0;i<n;i++)
+        for (int i=1;i<n;i++)
         {
-            System.out.print(a[i]);
+            if (a[i]==a[i+1])
+            {
+                if (a[i]!=a[i-1])
+                {
+                    System.out.print(a[i]+" ");
+                }
+            
+            }
+            else
+            count++;
+        }
+        if (count==n-1)
+        {
+            System.out.println("Unique");
         }
 	}
 }
