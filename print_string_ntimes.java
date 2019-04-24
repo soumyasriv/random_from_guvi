20b3263e073ce2256ab3771f85589010c0c266d8
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-24 09:27:12 -0700

    Create print_string_ntimes.java

---
+++ print_string_ntimes.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int n=sc.nextInt();
+		for (int i=0;i<=n-1;i++)
+		{
+		    System.out.println(s);
+		}
+	}
+}
