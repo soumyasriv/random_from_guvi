7cb4b1ea64b10676ca2c08a82b400fed42737f20
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-27 04:32:42 -0700

    Create even_pe_odd_viceversa.java

---
+++ even_pe_odd_viceversa.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for (int i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		for (int i=0;i<n;i++)
+		{
+		    if (i%2!=0)
+		    {
+		        if (a[i]%2==0)
+		        System.out.print(a[i]+" ");
+		    }
+		    else if (i%2==0)
+		    {
+		        if (a[i]%2!=0)
+		        System.out.print(a[i]+" ");
+		    }
+		    else
+		    continue;
+
+		}
+	}
+}
